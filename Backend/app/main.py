b699243e8f91c9be4888c896bf6777c302b87183
Erick Arrivillaga <e.arrivillaga15@gmail.com>
2024-09-01 01:07:54 -0600

    fix: Update CORS route in main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -5,9 +5,7 @@ from routes.hero import hero
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://example.com",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
